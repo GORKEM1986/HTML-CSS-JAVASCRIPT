3dcec4d077040624fb3e59263b41c54784bbdaf6
GORKEM1986 <aslanakbey7106@gmail.com>
2022-12-13 00:16:00 +0100

    variable

---
+++ JavaScrıpt/Variables Data Types/app.js
@@ -0,0 +1,78 @@
+console.log("***** Data Types ******");
+
+//?================== CONST================
+
+const fs = "Cohort 13";
+console.log(fs);
+
+const sayı = 20;
+console.log(sayı);
+console.log(typeof sayı);
+
+const sayı1 = "33";
+console.log(sayı1);
+console.log(typeof sayı1);
+
+// sayı1 = 55;
+
+// const pi;
+
+// pi = 3.14
+
+const s1 = "Hallo";
+const s2 = "FS13";
+console.log(s1 + s2);
+
+const n1 = "5";
+const n2 = "10";
+console.log(n1 + n2);
+
+//?===================== LET =====================
+
+let dil = "Javascript";
+console.log(dil);
+
+dil = "java";
+console.log(dil);
+
+dil = true;
+console.log(dil, typeof dil);
+
+dil = 3.14;
+console.log(dil, typeof dil);
+
+let cohort;
+console.log(cohort);
+
+cohort = 3;
+console.log(typeof cohort);
+
+// let cohort = 'FS13'
+
+let num1 = 5;
+let num2 = 3;
+let result = num1 + num2;
+console.log(++result);
+console.log(result);
+
+count = 55;
+console.log(count);
+count = 22;
+console.log(count);
+
+var x = 11;
+console.log(x);
+
+{
+  let a = 2;
+  const b = 3;
+  console.log(a, b);
+}
+// console.log (a, b)
+
+{
+  var c = 99;
+  console.log(c);
+}
+
+console.log(c);
