a730481ba936548cf825d76e2dc31076fed1b7d9
GORKEM1986 <aslanakbey7106@gmail.com>
2022-12-16 10:34:18 +0100

    karar yapıları

---
+++ JavaScrıpt/03-Conditions/conditions.js
@@ -0,0 +1,17 @@
+// * =======================================================
+// *             Karar Yapilari (Conditions)
+// * =======================================================
+
+console.log(" **** Conditions ****")
+
+//? ORNEK1: Console'dan girilen bir sayisinin pozitif,negatif
+//? yada 0 oldugunu tespit ederek yazdiriniz.
+
+// const num = Number(prompt("Please enter a number:"))
+// if (num > 0) {
+//   console.log(`${num} is posivite`)
+// } else if (num < 0) {
+//   console.log(`${num} is negative`)
+// } else {
+//   console.log(`${num} is equal to zero`)
+// }
