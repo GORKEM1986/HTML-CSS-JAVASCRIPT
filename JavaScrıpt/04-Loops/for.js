07881392a56fba7ec2fa232de9b9f1162a56fe5f
GORKEM1986 <aslanakbey7106@gmail.com>
2022-12-19 12:00:30 +0100

    while

--- JavaScrıpt/04-Loops/for.js
+++ JavaScrıpt/04-Loops/for.js
@@ -2,7 +2,7 @@
 // *             FOR LOOP
 // * =======================================================
 
-console.log(" **** FOR ****")
+// console.log(" **** FOR ****")
 
 // //? Ornek : 1 den n kadar sayıları toplayan kodu yazınız.
 
@@ -19,16 +19,46 @@ console.log(" **** FOR ****")
 
 
 
-//? Ornek: 0-100 arasında 10 adet rastgele tamsayığreten kodu 
-//? for dongusu ile yazınız..
+// //? Ornek: 0-100 arasında 10 adet rastgele tamsayığreten kodu 
+// //? for dongusu ile yazınız..
 
-const num = Number(prompt("Enter a number"))
+// const num = Number(prompt("Enter a number"))
 
-for ( let j = 1; j <= num; j++) {
+// for ( let j = 1; j <= num; j++) {
 
-    const randomNum = Math.round(Math.random() * 100) 
+//     const randomNum = Math.round(Math.random() * 100) 
 
-    console.log(`${j}. random number: ${randomNum}`)
+//     console.log(`${j}. random number: ${randomNum}`)
+// }
+
+//* NOT:
+//* Math.floor() =>en yakin alt tamsayiya yuvarlar
+//* Math.ceil() =>en yaki ust tamsayiya yuvarlar
+//* Math.round() => degerine gore yuvarlar.
+//* Math.trunc() => kesirli sayinin tam kismini alir.
+
+
+//? ÖRNEK: Girilen bir sayinin Asal olup olmadigini yazdiran kodu
+//? for dongulerini kullanarak yaziniz.
+
+const primeNum = Number(prompt("Enter a number"))
+
+//? Flag method
+let isPrime = true
+
+if (primeNum < 2) {
+  console.log("Number should be bigger than 1 ")
+} else {
+  for (let i = 2; i < primeNum; i++) {
+    if (primeNum % i === 0) {
+      //? Tam bolunuyorsa asal degildir
+      console.log(`${primeNum} is not a prime number-${i}`)
+      isPrime = false
+      break
+    }
+  }
+
+  if (isPrime === true) {
+    console.log(`${primeNum} is a prime number`)
+  }
 }
-
-
