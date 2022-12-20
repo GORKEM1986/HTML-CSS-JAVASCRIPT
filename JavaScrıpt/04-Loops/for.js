277b6ba9079c2b7f7c874f2035e987d39927544c
GORKEM1986 <aslanakbey7106@gmail.com>
2022-12-20 18:07:05 +0100

    while

--- JavaScrıpt/04-Loops/for.js
+++ JavaScrıpt/04-Loops/for.js
@@ -62,3 +62,21 @@ if (primeNum < 2) {
     console.log(`${primeNum} is a prime number`)
   }
 }
+
+
+
+// * ============================================
+// *            DONGULER(LOOPS)- WHILE
+// * ============================================
+
+console.log("****** WHILE *******")
+
+let count = 1
+while (count <= 10) {
+  console.log("Count:", count)
+  count++
+}
+
+//? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu while dongusu ile yaziniz.
