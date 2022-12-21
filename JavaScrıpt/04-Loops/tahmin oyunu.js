39c2ff5738d8cc0eab8f9fe394f271657cb250b3
GORKEM1986 <aslanakbey7106@gmail.com>
2022-12-21 16:57:51 +0100

    tahmin

---
+++ JavaScrıpt/04-Loops/tahmin oyunu.js
@@ -0,0 +1,75 @@
+// //? TAHMİN OYUNU//
+
+// const randomNumber = Math.trunc(Math.random() * 100)
+// let trying = 0
+// let enterNumber
+// let count = 0
+
+// do{
+//     count++
+//     trying++
+//     enterNumber = Number(prompt(`(0-100) olan sayıyı tahmin edin. ${count}. deneme:`)
+//     if (enterNumber < 0 || enterNumber > 100) {
+//         alert(`Lütfen 0-100 arasında bir sayı giriniz.)
+//     } else {
+//        if (enterNumber == randomNumber){
+//         alert (`Tebrikler sayıyı ${count} denemenizde tahmin ettiniz: ${randomNumber}`)
+//         console.log(`Tebrikler sayıyı tahmin ettiniz: ${randomNumber}`)
+//         break
+//        } else if(enterNumber > randomNumber){
+//         alert("Daha küçük rakam giriniz")
+//        } else if(enterNumber < randomNumber){
+//         alert("Daha büyük bir rakam giriniz")
+//        }
+//     }
+//     if (trying > 4) {
+//         let statusControl = prompt("Sayıyı bilemediniz devam etmek istiyor musunuz?(e/h): ")
+//         if (statusControl == 'e') {
+//             trying = 0
+//         } else {
+//             console.log(`Sayıyı bilemediniz. Sayı : ${randomNumber}`)
+//             break
+//         }
+
+//     }
+// } while (randomNumber != enterNumber) //Sayı tahmin edilemeyinceye kadar dön
+
+// Sayı Tahmin Oyunu
+const randomNumber = Math.trunc(Math.random() * 100);
+let trying = 0; //deneme hakkı (5 adet verildi if ile kontrol ettiriliyor)
+let enterNumber; //girilen sayı
+let count = 0; //sayaç
+do {
+  count++;
+  trying++;
+  enterNumber = +prompt(`(0-100) olan sayıyı tahmin edin. ${count}. deneme : `);
+  //sayının 0-100 arasında girilmesi kontrolü
+  if (enterNumber < 0 || enterNumber > 100) {
+    alert("Lütfen 0-100 arasında bir sayı giriniz..!");
+  } else {
+    //sayıyı tahmin etttiğimiz bölüm
+    if (enterNumber == randomNumber) {
+      alert(
+        `Tebrikler sayıyı ${count}. denemenizde tahmin ettiniz : ${randomNumber}`
+      );
+      console.log(`Tebrikler Sayıyı tahmin ettiniz : ${randomNumber}`);
+      break;
+    } else if (enterNumber > randomNumber) {
+      alert("Daha küçük bir rakam giriniz");
+    } else if (enterNumber < randomNumber) {
+      alert("Daha büyük bir rakam giriniz");
+    }
+  }
+  //5. denemeden sonra bize devam edip etmeyeceğimizi soruyor.
+  if (trying > 4) {
+    let statusControl = prompt(
+      "Sayıyı bilemediniz devam etmek istiyor musunuz?(e/h): "
+    );
+    if (statusControl == "e") {
+      trying = 0;
+    } else {
+      console.log(`Sayıyı bilemediniz. Sayı : ${randomNumber}`);
+      break;
+    }
+  }
+} while (randomNumber != enterNumber); //Sayı tahmin edilemeyinceye kadar dön
