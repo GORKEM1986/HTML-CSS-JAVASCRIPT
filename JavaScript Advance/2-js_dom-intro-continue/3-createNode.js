aa24ccac2e3a9357018ce21a2222d970ae4b34bc
GORKEM1986 <aslanakbey7106@gmail.com>
2023-01-16 09:33:05 +0100

    son

--- JavaScript Advance/2-js_dom-intro-continue/3-createNode.js
+++ JavaScript Advance/2-js_dom-intro-continue/3-createNode.js
@@ -67,4 +67,34 @@ console.log(myInput.value);
 newPar.id = "par";
 newPar.className = "center red";
 
-newPar.classList.add("bg-tomato");
+//? setAttribute
+newPar.setAttribute("id", "para"); //? alternative
+
+//? classList
+newPar.classList.add("bg-tomato", "left", "blue");
+newPar.classList.remove("left"); //? left classi silindi.
+
+if (!newPar.classList.contains("blue")) {
+  newPar.classList.add("blue");
+} else {
+  newPar.classList.add("orange");
+}
+//? toggle
+newPar.classList.toggle("white"); //? white class'i yoksa ekler. Veya varsa kaldirir.
+
+//? getAttribute()
+console.log(newPar.getAttribute("class"));
+console.log(newPar.getAttribute("id"));
+console.log(newPar.getAttribute("name")); //?null
+console.log(myInput.getAttribute("name")); //? languages
+console.log(myInput.getAttribute("type")); //? text
+
+//? alternative
+console.log(myInput.id);
+console.log(newPar.className);
+
+//? INNER HTML (Ciddi Guvenlik aciklari var)
+const newDiv = document.createElement("div");
+newDiv.id = "new-div";
+document.getElementById("btn").after(newDiv);
+newDiv.innerHTML += `<li id="go" class="red">Go</li>`;
